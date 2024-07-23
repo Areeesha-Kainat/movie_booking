52c26abdbceb0502d4d6fb62c7e3b1d1c2ba4151
Areesha Kainat <133228317+Areeesha-Kainat@users.noreply.github.com>
2024-07-23 18:24:11 +0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">AAA Cinema</h1>
+    </a> 
+</div>    
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2023 AAA  </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a href="contact-us.php">Contact</a>
+</div>
+
+<!-- <footer class="bg-dark d-flex justify-content-center">
+    <h2 class="text-white" href="#">COVID-19 LIVE STATS</h2>
+    <h2 style="color:#fff;">Developed By <a target="_blank" class="navbar-brand line text-white" href="https://github.com/aman05382/">Aman Sharma</a></h2>
+</footer> -->
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<div class="navbar-brand">
+    <a href="index.php">
+        <h1 class="navbar-heading">AAA Cinema</h1>
+    </a>
+</div>
+<div class="navbar-container">
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="schedule.php">Schedule</a></li>          
+            <li><a href="contact-us.php">Contact</a></li>
+        </ul>
+    </nav>
+</div>
